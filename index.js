b7f439fc494283486a72a8a8444f8bf996814613
Ivan <listepo@ya.ru>
2015-04-18 00:10:43 +0300

    Init

---
+++ index.js
@@ -0,0 +1,53 @@
+var raven = require('raven');
+
+module.exports = function(sails) {
+
+  return {
+
+    /**
+     * Default configuration
+     *
+     * We do this in a function since the configuration key for
+     * the hook is itself configurable, so we can't just return
+     * an object.
+     */
+    defaults: {
+
+      __configKey__: {
+        // Set autoreload to be active by default
+        active: true,
+        dsn: null,
+        level: 'error'
+      }
+    },
+
+    /**
+     * Initialize the hook
+     * @param  {Function} cb Callback for when we're done initializing
+     */
+    initialize: function(cb) {
+
+      if (!sails.config[this.configKey].active) {
+        sails.log.verbose("Autoreload hook deactivated.");
+        return cb();
+      }
+
+      var client = new raven.Client(
+          sails.config[this.configKey].dsn,
+          {
+              level: sails.config[this.configKey].level
+          }
+          );
+
+      sails.sentry = client;
+      sails.on('router:request:500', function(err) {
+          client.captureError(err);
+      });
+      // We're done initializing.
+      return cb();
+
+    },
+
+  };
+
+};
